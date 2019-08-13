d7b93a649c552e6e72d7849923fb85177d0d19f8
sumit.templatic@gmail.com <aquaris10>
2019-08-13 18:29:18 +0530

    Project Uploaded

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: [
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+                    'node_modules/moment/min/moment.min.js',
+                    'node_modules/fontawesome/js/all.min.js',
+                    'node_modules/datatables.net/js/*min.js',
+                    'node_modules/datatables.net-bs4/js/*min.js',
+                    'node_modules/datatables.net-buttons/js/*.buttons.min.js',
+                ],
+                dest: 'dist/app.min.js',
+            },
+            js2: {
+                src: [
+                    'node_modules/jszip/dist/*min.js',
+                    'node_modules/pdfmake/build/pdfmake.min.js',
+                    'node_modules/pdfmake/build/vfs_fonts.js',
+                ],
+                dest: 'dist/app2.min.js'
+            },
+            js3: {
+                src: [
+                    'node_modules/datatables.net-buttons/js/buttons.colVis.min.js',
+                    'node_modules/datatables.net-buttons/js/buttons.html5.min.js',
+                    'node_modules/datatables.net-buttons/js/buttons.print.min.js',
+                    'node_modules/datatables.net-buttons-bs4/js/*min.js',
+                    'node_modules/datatables.net-colreorder/js/*min.js',
+                    'node_modules/datatables.net-colreorder-bs4/js/*min.js',
+                    'node_modules/datatables.net-fixedcolumns/js/*min.js',
+                    'node_modules/datatables.net-fixedcolumns-bs4/js/*min.js',
+                    'node_modules/datatables.net-fixedheader/js/*min.js',
+                    'node_modules/datatables.net-fixedheader-bs4/js/*min.js',
+                    'node_modules/datatables.net-keytable/js/*min.js',
+                    'node_modules/datatables.net-keytable-bs4/js/*min.js',
+                    'node_modules/datatables.net-responsive/js/*min.js',
+                    'node_modules/datatables.net-responsive-bs4/js/*min.js',
+                    'node_modules/datatables.net-rowgroup/js/*min.js',
+                    'node_modules/datatables.net-rowgroup-bs4/js/*min.js',
+                    'node_modules/datatables.net-rowreorder/js/*min.js',
+                    'node_modules/datatables.net-rowreorder-bs4/js/*min.js',
+                    'node_modules/datatables.net-scroller/js/*min.js',
+                    'node_modules/datatables.net-scroller-bs4/js/*min.js',
+                    'node_modules/datatables.net-select/js/*min.js',
+                    'node_modules/datatables.net-select-bs4/js/*min.js',
+                    'node_modules/datatables.net-autofill/js/*min.js',
+                    'node_modules/datatables.net-autofill-bs4/js/*min.js',
+                ],
+                dest: 'dist/app3.min.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: ['dist/app2.min.js'],
+                dest: 'dist/app2.min.js',
+            },
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/theme.min.css': [
+                        'node_modules/fontawesome/css/all.min.css',
+                        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                        'node_modules/datatables.net-bs4/css/*min.css',
+                        'node_modules/datatables.net-autofill-bs4/css/*min.css',
+                        'node_modules/datatables.net-buttons-bs4/css/*min.css',
+                        'node_modules/datatables.net-colreorder-bs4/css/*min.css',
+                        'node_modules/datatables.net-fixedcolumns-bs4/css/*min.css',
+                        'node_modules/datatables.net-fixedheader-bs4/css/*min.cc',
+                        'node_modules/datatables.net-keytable-bs4/css/*min.css',
+                        'node_modules/datatables.net-responsive-bs4/css/*min.css',
+                        'node_modules/datatables.net-rowgroup-bs4/css/*min.css',
+                        'node_modules/datatables.net-rowreorder-bs4/css/*min.css',
+                        'node_modules/datatables.net-scroller-bs4/css/*min.css',
+                        'node_modules/datatables.net-select-bs4/css/*min.css',
+                    ],
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/moment/min/moment.min.js',
+                    'node_modules/fontawesome/js/all.min.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+                ],
+                tasks: ['concat', 'uglify', 'cssmin'],
+            },
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('develop', ['concat', 'uglify', 'cssmin']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# sumit-boostrap-boilerplate
-Bootstrap Boilerplate with lots of plugin integration
+# Welcome to Bootstrap Boilerplate!
+
+ **Bootstrap Boilerplate** is the boostrap ready distribution with dependency of **JQuery**,**Datatables**,**MomentJS**.
+
+# Files
+
+## CSS Distribution Folder
+
+CSS file is located on ***dist/css*** folder
+
+    dist/css/theme.min.css
+
+## JS Distribution Folder
+
+JS files are located on ***dist/js*** folder
+
+    dist/js/app.min.js
+    dist/js/app2.min.js
+    dist/js/app3.min.js
+
+# Installation
+
+## CSS Installation
+    <link  rel="stylesheet" href="dist/css/theme.min.css">
+
+## JS Installation
+    <script  src="dist/js/app.min.js"></script>
+    <script  src="dist/js/app2.min.js"></script>
+    <script  src="dist/js/app3.min.js"></script>
+> OR
+## JSDelivr CDN
+### CSS CDN
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sumit-bootstrap-boilerplate@1.0.5/dist/theme.min.css"/>
+### CSS CDN
+    <script src="https://cdn.jsdelivr.net/npm/sumit-bootstrap-boilerplate@1.0.5/dist/app.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sumit-bootstrap-boilerplate@1.0.5/dist/app2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sumit-bootstrap-boilerplate@1.0.5/dist/app3.min.js"></script>
+
+### Demo File
+
+open ***index.html*** in browser.
+
+## Comming Soon
+>Bootstrap theme switcher feature in which you can use the below free bootstrap theme:
+
+ - Journal
+ - Litera
+ - Cosmo
+ - Cerulean
+ - Flatly
+ - Materia
+ - Cyborg
+ - Lumen
+ - Spacelab
+ - Minty
+ - Sandstone
+ - Slate
+ - Simplex
+ - Pulse
+ - Superhero
+ - Sketchy
+ - Solar
+ - United
+
+>Also, you can include your own boostrap theme and include in my boilerplate.
+>Customization bootstrap is available on [Boostrap Build](https://bootstrap.build/themes) .
+>I am going to include third party plugins: [SweetAlert](https://sweetalert2.github.io/) ,[IziToast](http://izitoast.marcelodolza.com/), [JqueryValidation](https://jqueryvalidation.org) and many more.
